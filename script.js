331419228fff7a704f0db74b26c5166d88535ee2
BojanapalliRajyaLaxmi <rajyalakshmiraji124@gmail.com>
2024-10-02 23:01:20 +0530

    loops

---
+++ script.js
@@ -0,0 +1,174 @@
+// Task 1: Print Numbers from 1 to 10
+// Write a for loop that prints all the numbers from 1 to 10, each on a new line.
+for (var i=1;i<101;i++){
+        console.log(i);
+    }
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+    11
+    12
+    13
+    14
+    15
+    16
+    17
+    18
+    19
+    20
+    21
+    22
+    23
+    24
+    25
+    26
+    27
+    28
+    29
+    30
+    31
+    32
+    33
+    34
+    35
+    36
+    37
+    38
+    39
+    40
+    41
+    42
+    43
+    44
+    45
+    46
+    47
+    48
+    49
+    50
+    51
+    52
+    53
+    54
+    55
+    56
+    57
+    58
+    59
+    60
+    61
+    62
+    63
+    64
+    65
+    66
+    67
+    68
+    69
+    70
+    71
+    72
+    73
+    74
+    75
+    76
+    77
+    78
+    79
+    80
+    81
+    82
+    83
+    84
+    85
+    86
+    87
+    88
+    89
+    90
+    91
+    92
+    93
+    94
+    95
+    96
+    97
+    98
+    99
+    100
+    // -----------------------------------------------------------------
+    // Task 2: Print Multiplication Table
+    // Create a for loop that prints the multiplication table for the number 5, from 1 to 10. For example, it should print 5 x 1 = 5, 5 x 2 = 10, and so on up to 5 x 10 = 50.
+    num=5;
+    for (var i=1;i<=10;i++){
+        console.log(num+"*"+i+"="+num*i);
+    }
+    // output:
+    // 5*1=5
+    // 5*2=10
+    // 5*3=15
+    // 5*4=20
+    // 5*5=25
+    // 5*6=30
+    // 5*7=35
+    // 5*8=40
+    // 5*9=45
+    // 5*10=50
+    // ------------------------------------------------------------------------
+    // Task 3: Sum of First 10 Natural Numbers
+    // Use a for loop to calculate and print the sum of the first 10 natural numbers (1 to 10).
+    sum=0;
+    for(var i=1;i<=10;i++){
+        sum+=i;
+    }
+    console.log(sum);
+    // output:
+    // -->55
+    // --------------------------------------------------------------------------------
+    // Task 4: Print Numbers Divisible by 3 from 1 to 30
+    // Create a for loop that prints all numbers between 1 and 30 that are divisible by 3.
+    j=3;
+    while(j<=30){
+        console.log(j);
+        j+=3;
+    }
+    // output:
+    // 3
+    // 6
+    // 9
+    // 12
+    // 15
+    // 18
+    // 21
+    // 24
+    // 27
+    // 30
+    // ---------------------------------------------------------------------------------------
+    // Task 5: Print Square of Numbers from 1 to 10
+    // Write a for loop that prints the square of each number from 1 to 10. For example, it should print 1 squared is 1, 2 squared is 4, and so on up to 10 squared is 100.
+    i=1;
+    while(i<=10){
+        console.log(i, 'squared is' , i**2);
+        i++;
+    }
+    // output:
+    // 1 squared is 1
+    // 2 squared is 4
+    // 3 squared is 9
+    // 4 squared is 16
+    // 5 squared is 25
+    // 6 squared is 36
+    // 7 squared is 49
+    // 8 squared is 64
+    // 9 squared is 81
+    // 10 squared is 100
+    
+    
+    
